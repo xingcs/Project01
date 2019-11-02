3a519c31cf6ba3a6aaf567c5ee54884dc67b48a3
xingcs <1029288680@qq.com>
2019-11-02 17:31:55 +0800

    Initial commit

---
+++ src/com/whir/ideaProject/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.whir.ideaProject;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    // 新的注释
+    public static String getStr(){
+        System.out.println("hello idea");
+        String o = null;
+        return o;
+
+    }
+}
---
+++ src/com/whir/test.java
@@ -0,0 +1,11 @@
+package com.whir;
+
+/**
+ * @author xingcs
+ * @date 2019/11/2 - 10:40
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
